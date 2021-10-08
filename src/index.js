7c235da24a08a771a47305c3156e19ce30765657
Aleksandr Kulyk <ale.kulyk@gmail.com>
2021-10-08 22:47:25 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './sass/main.scss';
+//import * as bootstrap from 'bootstrap';
+
+const buttonLoad = document.querySelector('button.load-more');
+console.log(buttonLoad);
+buttonLoad.disabled = true;
+
+const options = {
+    key: '23761306-59ca6a0f0608395e39c81a3c2',
+    q: '',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true
+}
+console.log(options);
