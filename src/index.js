e6b4a14b8c9fb9beb2de9946808f07e7e6cb3eac
Aleksandr Kulyk <ale.kulyk@gmail.com>
2022-02-14 01:32:37 +0200

    changed index.js

--- src/index.js
+++ src/index.js
@@ -121,9 +121,7 @@ function makePicturiesList (newCards) {
           <p class="info-item">
             <b>Comments ${item.comments}</b>
           </p>
-          <p class="info-item">
-            <b>Downloads ${item.downloads}</b>
-          </p>
+          
         </div>
         </div>`
       })
